71f46839b7908fe597783368309a54a847f2fef0
Unknown <Appietro516@email.arizona.edu>
2018-09-18 07:54:04 -0700

    added player movement script

---
+++ project1/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    
+    public Transform player;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal =  Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed  * Time.deltaTime;
+        
+        //since the game is top down, the player should control horizontal and vertical movement.
+        player.position += new Vector3(horizontal, vertical, 0);
+        
+		
+	}
+}
