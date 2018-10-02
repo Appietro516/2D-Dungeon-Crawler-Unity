855d181e38f5b1a1a3ce37ae4f50fcbff0732cc7
Unknown <Appietro516@email.arizona.edu>
2018-10-02 15:06:19 -0700

    added some animation, sound, particles

--- project1/Assets/scripts/PlayerInput.cs
+++ project1/Assets/scripts/PlayerInput.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class PlayerInput : MonoBehaviour {
     
     public Transform player;
     public float speed;
+    public Animator sword_anim;
+    public AudioClip sword_sound;
+    public AudioSource sound_player;
     
     private bool attack_state = false;
 
 	// Use this for initialization
 	void Start () {
-		
+        sword_anim.Play("idle");
 	}
 	
 	// Update is called once per frame
@@ -20,6 +25,7 @@ public class PlayerInput : MonoBehaviour {
         float vertical = Input.GetAxis("Vertical") * speed  * Time.deltaTime;
         bool attack = Input.GetAxis("Fire1")  > 0f;
         
+        
         Vector3 deltaMove = new Vector3(horizontal, vertical, 0);
         
         
@@ -27,10 +33,23 @@ public class PlayerInput : MonoBehaviour {
         player.position += deltaMove;
         
         
+        if (vertical != 0){
+            player.localScale = new Vector3(player.localScale.x, Math.Sign(-1 * vertical) * Math.Abs(player.localScale.y),  player.localScale.z);
+        }
+        
+        if (horizontal != 0){
+            player.localScale = new Vector3(Math.Sign(-1 * horizontal) * Math.Abs(player.localScale.x), player.localScale.y,  player.localScale.z);
+        }
+        
+    
         
         if (attack && !attack_state){
-            print("attack!");
+             sword_anim.Play("sword_swing");
+             sound_player.PlayOneShot(sword_sound);
         }
+        
+        
+        
         
         attack_state = attack;
         
